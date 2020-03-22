7ddbadb87952b46c6ff2bd8c03751b3efed49887
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-22 10:17:18 +0900

    hotels finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :hotels, only: [:index, :show] do
-    resources :room_categories, only: [:index, :show] do
-      resources :rooms, only: [:index, :show]
-    end
+  resources :hotels, only: [:index, :show]
+  resources :room_categories, only: [:index, :show] do
+    resources :rooms, only: [:index, :show]
   end
   resources :workers, only: [:index]
   resources :amenities, only: [:index]
