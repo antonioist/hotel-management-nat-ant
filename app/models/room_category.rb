8970a1435461f95302097b54df4d72360c7e3cad
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-22 11:21:20 +0900

    fixed room_category routes and controller finished

--- app/models/room_category.rb
+++ app/models/room_category.rb
@@ -1,5 +1,6 @@
 class RoomCategory < ApplicationRecord
   has_many :rooms
+  belongs_to :hotel
   validates :category, :price, presence: true
   validates :price, numericality: {only_integer: true}
 end
