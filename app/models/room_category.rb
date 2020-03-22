281cb0c977b893303f9232740cd884b288d5b5d1
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-03-22 11:58:28 +0900

    routes totally fixed

--- app/models/room_category.rb
+++ app/models/room_category.rb
@@ -1,6 +1,5 @@
 class RoomCategory < ApplicationRecord
   has_many :rooms
-  belongs_to :hotel
   validates :category, :price, presence: true
   validates :price, numericality: {only_integer: true}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get 'rooms/show'
   devise_for :users
   root to: 'pages#home'
-  resources :hotels, only: [:index, :show]
-  resources :room_categories, only: [:index, :show] do
+  resources :hotels, only: [:index, :show] do
     resources :rooms, only: [:index, :show]
+    resources :workers, only: [:index]
   end
-  resources :workers, only: [:index]
+  resources :room_categories, only: [:index, :show]
   resources :amenities, only: [:index]
   resources :menu_items, only: [:index]
   resources :bookings, only: [:new, :create]
